0cc33c23761d6c3bcbb95beaa726db382bbb9e1c
Alex <alex_p97@live.com>
2025-09-30 13:14:20 +0100

    Updated comments

--- models.py
+++ models.py
@@ -3,15 +3,15 @@ from typing import List, Optional
 
 # Use dataclass decorator to generate boilerplate code for the class
 @dataclass
+class LinkedImage:
+    image_path: str
+    link_url: Optional[str] = None
+    alt_text: Optional[str] = None
+    
+@dataclass
 class NewsletterSection:
     heading: str
     subheading: Optional[str] = None
     body: str = ""
     anchor_id: str = ""
     images: List[LinkedImage] = field(default_factory=list)
-
-@dataclass
-class LinkedImage:
-    image_path: str
-    link_url: Optional[str] = None
-    alt_text: Optional[str] = None
